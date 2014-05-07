03e6a4dbdf5594e6050eb337bb1d16fcbb7bc5d5
marchorse <marchorse@163.com>
2014-05-07 09:50:07 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using DotNet.Utilities;
+using System.Media;
+
+namespace wavHandler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MediaHandler m = new MediaHandler();
+            MediaHandler.SyncPlayWAV("D:\\Users\\marc\\Documents\\Visual Studio 2012\\Projects\\wavHandler\\wavHandler\\test.wav");
+            
+            Console.WriteLine("Hello Media Wav!");
+        }
+    }
+}
